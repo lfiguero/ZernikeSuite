ce04de6066d1974e6edf0ecda8b5fd53e7ad1ca2
Leonardo Figueroa <leonardo@leonardofigueroa.org>
2017-08-04 21:53:53 -0400

    Separated Sobolev orthogonal polynomials generation and testing
    And started writing a formula-finding test.

--- undocumented_experiments/SobolevOrthogonalPolynomials.jl
+++ undocumented_experiments/SobolevOrthogonalPolynomials.jl
@@ -36,48 +36,51 @@ function recombine(coll::Array{ZernikeSuite.ZFun,1})
     newcoll
 end
 
-function SturmLiouvilleTest(α::Real, maxdeg::Integer, normalizeFlag::Bool, recombineFlag::Bool)
-    @assert α > -1
-    @assert maxdeg ≥ 0
-    # We start with basis which is weighted L²-orthogonal
-    basis = [ZernikePoly(α, i, k-i) for k in 0:maxdeg for i in 0:k]
-    dim = length(basis)
-    # We turn the basis into a weighted Sobolev-orthogonal one via a
-    # Gram–Schmidt process
-    squaredNorm = Array{Float64}(dim)
-    for i = 1:dim
-	for j = 1:i-1
-		basis[i] = basis[i] - sip(basis[i], basis[j])/squaredNorm[j] * basis[j]
+function SOP(α::Real, maxdeg::Integer, normalizeFlag::Bool=false, recombineFlag::Bool=false)
+	@assert α > -1
+	@assert maxdeg ≥ 0
+	# We start with basis which is weighted L²-orthogonal
+	basis = [ZernikePoly(α, i, k-i) for k in 0:maxdeg for i in 0:k]
+	dim = length(basis)
+	# We turn the basis into a weighted Sobolev-orthogonal one via a
+	# Gram–Schmidt process
+	squaredNorm = Array{Float64}(dim)
+	for i = 1:dim
+		for j = 1:i-1
+			basis[i] = basis[i] - sip(basis[i], basis[j])/squaredNorm[j] * basis[j]
+		end
+		if normalizeFlag
+			basis[i] = basis[i]/sqrt(sip(basis[i],basis[i]))
+			squaredNorm[i] = 1.0
+		else
+			squaredNorm[i] = real(sip(basis[i], basis[i]))
+		end
 	end
-	if normalizeFlag
-		basis[i] = basis[i]/sqrt(sip(basis[i],basis[i]))
-		squaredNorm[i] = 1.0
-	else
-		squaredNorm[i] = real(sip(basis[i], basis[i]))
+	# Intra-degree recombinations
+	if recombineFlag
+		for k = 0:maxdeg
+			rng = ZernikeSuite.positionRange(k)
+			basis[rng] = recombine(basis[rng])
+		end
 	end
-    end
-    # Intra-degree recombinations
-    if recombineFlag
-	    for k = 0:maxdeg
-		rng = ZernikeSuite.positionRange(k)
-		basis[rng] = recombine(basis[rng])
-	    end
-    end
-    A = Array{Complex128}(dim, dim)
-    B = Array{Complex128}(dim, dim)
-    for i = 1:dim
-	for j = 1:dim
-	    A[i,j] = bf(basis[i], basis[j])
-	    B[i,j] = sip(basis[i], basis[j])
-	end
-    end
-    bf_orthogonality_test = norm(A-diagm(diag(A)))/norm(A)
-    sip_orthogonality_test = norm(B-diagm(diag(B)))/norm(B)
-    eigenvalues = diag(A) ./ diag(B)
-    return bf_orthogonality_test, sip_orthogonality_test, basis, eigenvalues
+	return basis
 end
 
-SturmLiouvilleTest(α::Real, maxdeg::Integer) = SturmLiouvilleTest(α, maxdeg, false, false)
+function SturmLiouvilleTest(α::Real, maxdeg::Integer, normalizeFlag::Bool=false, recombineFlag::Bool=false)
+	basis = SOP(α, maxdeg, normalizeFlag, recombineFlag)
+	A = Array{Complex128}(length(basis), length(basis))
+	B = Array{Complex128}(length(basis), length(basis))
+	for i = 1:length(basis)
+		for j = 1:length(basis)
+			A[i,j] = bf(basis[i], basis[j])
+			B[i,j] = sip(basis[i], basis[j])
+		end
+	end
+	bf_orthogonality_test = norm(A-diagm(diag(A)))/norm(A)
+	sip_orthogonality_test = norm(B-diagm(diag(B)))/norm(B)
+	eigenvalues = diag(A) ./ diag(B)
+	return bf_orthogonality_test, sip_orthogonality_test, eigenvalues
+end
 
 function relativeComponentWeight(f::ZFun)
     weights = ZernikeSuite.h(f.α, f.degree)
@@ -89,3 +92,10 @@ function relativeComponentWeight(f::ZFun)
     out = sqrt(out/sum(out))
 end
 
+function coefficientFormulaSearch(α::Real, maxdeg::Integer)
+	basis = SOP(α, maxdeg)
+	w = -[basis[ZernikeSuite.positionRange(deg)[i+1]].coefficients[ZernikeSuite.positionRange(deg-2)[i]] for deg in 3:maxdeg for i in 1:deg-1]
+	d = [deg for deg in 3:maxdeg for i in 1:deg-1]
+	md = [abs(2*i-deg) for deg in 3:maxdeg for i in 1:deg-1]
+	return (w, d, md)
+end
